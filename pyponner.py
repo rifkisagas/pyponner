3a0dc8bb1cc208df47e1b8ff1532ae86fdea7446
Rifki Arisagas <77217948+rifkisagas@users.noreply.github.com>
2022-08-10 05:02:05 -0400

    Add files via upload

--- pyponner.py
+++ pyponner.py
@@ -2,8 +2,7 @@ import socket
 import pyfiglet
 import termcolor
 import signal
-import os
-import argparse
+from ping import Ping
 import time
 word = pyfiglet.figlet_format("PyPonner")
 
@@ -12,26 +11,40 @@ class status:
 
 def desc():
     print("\nPyPonner is a simple program used to scan active ports of the targets inputed.\n")
+
 def exitfunc():
     print('\nProgram Exit...')   
     exit(1)
+
+def error_input():
+    time.sleep(2)
+    main()
+
 def check_ip(input):
     if '.' in input:
         if input.strip().isdigit():
             return
     else:
-        print(termcolor.colored(("[!]Invalid target input."),'red'))
-        time.sleep(2)
-        main()
+        print(termcolor.colored(("[!] Invalid target input."),'red'))
+        error_input()
+
 def handler(signum, frame):
     exitfunc()
+
 def scan(target, ports):
     status.x = 0
     print('\n' + '[*] Starting Scan For ' + str(target))
-    for port in range(1, ports):
-        scan_port(target, port)
-    if (status.x == 0) :
-        print(termcolor.colored(("[-] No Port Opened!"), 'red'))
+    ping = Ping(target)
+    if ping.returncode < 1:
+        for port in range(1, ports):
+            scan_port(target, port)
+        if (status.x == 0) :
+            print(termcolor.colored(("[-] No Port Opened!"), 'red'))
+    else :
+        if target.replace('.','').isdigit():
+            print(termcolor.colored((f"[-] Target : {target} is unreachable!"), 'red'))
+        else :
+            print(termcolor.colored((f"[-] Target : {target} is not an IP Address!"), 'red'))
 
 
 def scan_port(ipaddress, port):
@@ -48,27 +61,27 @@ def scan_port(ipaddress, port):
 def main():
     print('\033c')
     print(word)
-    # parser = argparse.ArgumentParser(description='Used for scanning active ports of the targets.')
-    # parser.parse_args()    
-          # prog='pyponner.py',
-          # description='Used for scanning active ports of the targets.',
-          # epilog)
     print("\nMultiple target example : 192.168.56.1,192.168.56.2")
     targets = input("[+] Enter Targets (split them by ,): ")
     check_ip(targets)
-    print("The inputed ports perform as many iteration as input")
-    ports = int(input("[*] Enter How Many Ports You Want To Scan: "))
-    if ',' in targets:
-        print(termcolor.colored(("[*] Scanning Multiple Targets"), 'green'))
-        for ip_addr in targets.split(','):
-            scan(ip_addr.strip(' '), ports)
+    print("\nThe inputed ports perform as many iteration as input")
+    ports = int(input("[*] Enter How Many Ports You Want To Scan (Max : 65535): "))
+    if ports > 65535:
+        print(termcolor.colored(('[-] Maximum port input reached!'),'red'))
+        error_input()
     else:
-        scan(targets, ports)
-    ask = input("[?] Do you want to scan again? (y/n): ")
-    if "y" in ask:
-        print('\033c')
-        return main()
-    else:
-        exitfunc()
+        if ',' in targets:
+            print(termcolor.colored(("[*] Scanning Multiple Targets"), 'green'))
+            for ip_addr in targets.split(','):
+                scan(ip_addr.strip(' '), ports)
+        else:
+            scan(targets, ports)
+        ask = input("[?] Do you want to scan again? (y/n): ")
+        if "y" in ask:
+            print('\033c')
+            return main()
+        else:
+            exitfunc()
+
 signal.signal(signal.SIGINT,handler)
 main()
